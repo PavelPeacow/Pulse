b1f95feed204f5ac4b5f55e83682fa4f7014978c
Pavel <kaygorodow1@mail.ru>
2020-08-01 16:46:50 +0500

    step_6

--- src/js/script.js
+++ src/js/script.js
@@ -33,8 +33,6 @@ $(document).ready(function () {
       }
    );
 
-
-
    function toggleSlide(item) {
       $(item).each(function (i) {
          $(this).on("click", function (e) {
@@ -51,4 +49,22 @@ $(document).ready(function () {
 
    toggleSlide(".catalog-item__link");
    toggleSlide(".catalog-item__back");
+
+   //modal
+
+   $("[data-modal=consultation]").on("click", function () {
+      $(".overlay, #consultation").fadeIn("slow");
+   });
+   $(".modal__close").on("click", function () {
+      $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+   });
+
+   $(".button-mini").each(function (i) {
+      $(this).on("click", function () {
+         $("#order .modal__descr").text(
+            $(".catalog-item__subtitle").eq(i).text()
+         );
+         $(".overlay, #order").fadeIn("slow");
+      });
+   });
 });
